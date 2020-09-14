3e1c14557933ab12f63d77b2b61180202cad2478
chiujienwu <jchiu1@my.wctc.edu>
2020-09-13 23:47:17 -0500

    Initial commit

---
+++ src/main/java/edu/wctc/sbassign/controller/WepAppControler.java
@@ -0,0 +1,52 @@
+package edu.wctc.sbassign.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+@Controller
+public class WepAppControler {
+    private String appMode;
+
+    @Autowired
+    public void WebAppContoller(Environment environment){
+        appMode = environment.getProperty("app-mode");
+    }
+
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("datetime", new Date());
+        model.addAttribute("projectname", "Unit3");
+        model.addAttribute("mode", appMode);
+        model.addAttribute("blogname", "Jerry's Big Bad Blog of the Dark Dark Web");
+        model.addAttribute("source","https://www.theverge.com/2013/12/19/5183356/how-to-steal-bitcoin-in-three-easy-steps");
+
+        return "index";
+    }
+
+    @RequestMapping("/bitcoin1")
+    public String lesson1tutorial(Model model){
+        model.addAttribute("blogname","History of Grand Theft Bitcoin!");
+        return "pages/introtutorial";
+    }
+
+    @RequestMapping("/bitcoin2")
+    public String lesson2tutorial(Model model){
+        model.addAttribute("blogname","Lesson 1. Copying the Keys");
+        return "pages/lesson1tutorial";
+    }
+
+    @RequestMapping("/bitcoin3")
+    public String lesson3tutorial(){
+        return "pages/lesson2tutorial";
+    }
+
+    @RequestMapping("/bitcoin4")
+    public String lesson4tutorial(){
+        return "pages/finaltutorial";
+    }
+}
---
+++ src/test/java/edu/wctc/sbassign/SbassignApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wctc.sbassign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbassignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
