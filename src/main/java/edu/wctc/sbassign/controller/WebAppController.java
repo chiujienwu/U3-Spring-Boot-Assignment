93bc5030bb4e9b590f1fb64b8c1e1ce08538efcd
chiujienwu <jchiu1@my.wctc.edu>
2020-09-14 02:44:35 -0500

    corrected controller file name spelling

--- src/main/java/edu/wctc/sbassign/controller/WebAppController.java
+++ src/main/java/edu/wctc/sbassign/controller/WebAppController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Date;
 
 @Controller
-public class WepAppController {
+public class WebAppController {
     private String appMode;
 
     @Autowired
